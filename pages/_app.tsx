1aff2d9a7441768e6663abd732774846f56ac304
hisomura <hisomura.noreply@gmail.com>
2020-10-06 07:16:59 +0900

    Fix _app.tsx types.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import "../src/styles/global.css";
-import AppProps from "next/app";
 import { Provider } from "react-redux";
 import store from "../src/store/store";
+import { AppPropsType } from "next/dist/next-server/lib/utils";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppPropsType) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
